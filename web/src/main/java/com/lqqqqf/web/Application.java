aae9bb098985d3c92b0c9613de1c85f96acdb701
apple <471390946@qq.com>
2016-06-13 22:08:11 +0800

    增加spring-boot

---
+++ web/src/main/java/com/lqqqqf/web/Application.java
@@ -0,0 +1,15 @@
+package com.lqqqqf.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by apple on 16/6/13.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
